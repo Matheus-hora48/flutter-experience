929374af9826daad8af5bc92b09b65412920d8d7
Matheus Hora <matheus11hora@gmail.com>
2024-01-30 14:58:34 -0300

    finalizando a mensagem

--- lab_clinicas_core/lib/src/helpers/messages.dart
+++ lab_clinicas_core/lib/src/helpers/messages.dart
@@ -28,6 +28,53 @@ final class Messages {
 
 mixin MessageStateMixin {
   final Signal<String?> _erroMessage = signal(null);
+  String? get erroMessage => _erroMessage();
+
+  final Signal<String?> _infoMessage = signal(null);
+  String? get infoMessage => _erroMessage();
+
+  final Signal<String?> _successMessage = signal(null);
+  String? get successMessage => _erroMessage();
+
+  void clearError() => _erroMessage.value = null;
+  void clearInfo() => _erroMessage.value = null;
+  void clearSuccess() => _erroMessage.value = null;
+
+  void showError(String message) {
+    untracked(() => clearError());
+    _erroMessage.value = message;
+  }
+
+  void showInfo(String message) {
+    untracked(() => clearInfo());
+    _infoMessage.value = message;
+  }
+
+  void showSuccess(String message) {
+    untracked(() => clearSuccess());
+    _successMessage.value = message;
+  }
+
+  void clearAllMessages() {
+    untracked(() {
+      clearError();
+      clearInfo();
+      clearSuccess();
+    });
+  }
 }
 
-mixin MessageViewMixin<T extends StatefulWidget> on State<T> {}
+mixin MessageViewMixin<T extends StatefulWidget> on State<T> {
+  void messageListener(MessageStateMixin state) {
+    effect(() {
+      switch (state) {
+        case MessageStateMixin(:final erroMessage?):
+          Messages.showError(erroMessage, context);
+        case MessageStateMixin(:final infoMessage?):
+          Messages.showInfo(infoMessage, context);
+        case MessageStateMixin(:final successMessage?):
+          Messages.showSucess(successMessage, context);
+      }
+    });
+  }
+}
