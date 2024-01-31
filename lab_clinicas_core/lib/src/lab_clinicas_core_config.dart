529d9ca9b31ef926537a5297e6884ddfad86435f
Matheus Hora <matheus11hora@gmail.com>
2024-01-31 01:25:23 -0300

    implementando o flutter get_it

---
+++ lab_clinicas_core/lib/src/lab_clinicas_core_config.dart
@@ -0,0 +1,47 @@
+import 'package:asyncstate/asyncstate.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:lab_clinicas_core/src/loader/lab_clinica_loader.dart';
+
+class LabClinicasCoreConfig extends StatelessWidget {
+  const LabClinicasCoreConfig({
+    super.key,
+    this.binding,
+    this.pages,
+    this.pagesbuilders,
+    this.modules,
+    required this.title,
+  });
+
+  final ApplicationBindings? binding;
+  final List<FlutterGetItPageRouter>? pages;
+  final List<FlutterGetItPageBuilder>? pagesbuilders;
+  final List<FlutterGetItModule>? modules;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterGetIt(
+      debugMode: kDebugMode,
+      bindings: binding,
+      bindingsBuilder: () => [],
+      pages: [...pages ?? [], ...pagesbuilders ?? []],
+      builder: (context, routes, flutterGetItNavObserver) {
+        return AsyncStateBuilder(
+          loader: LabClinicasLoader(),
+          builder: (navigatorObserver) {
+            return MaterialApp(
+              navigatorObservers: [
+                navigatorObserver,
+                flutterGetItNavObserver,
+              ],
+              routes: routes,
+              title: title,
+            );
+          },
+        );
+      },
+    );
+  }
+}
