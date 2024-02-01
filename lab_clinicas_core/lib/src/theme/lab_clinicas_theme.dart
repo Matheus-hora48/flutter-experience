910c96bb5618e94a1f569cfa9ca671345e06a514
Matheus Hora <matheus11hora@gmail.com>
2024-01-31 23:51:55 -0300

    finalizando o thema

--- lab_clinicas_core/lib/src/theme/lab_clinicas_theme.dart
+++ lab_clinicas_core/lib/src/theme/lab_clinicas_theme.dart
@@ -6,6 +6,10 @@ final class LabClinicasTheme {
   static const blueColor = Color(0xFF01BDD6);
   static const lightGreyColor = Color(0xFFFFEFE9);
 
+  static const darkBackgroundColor = Color(0xFF121212);
+  static const darkCardColor = Color(0xFF1E1E1E);
+  static const darkTextColor = Colors.white;
+
   static final _defaultInputBorder = OutlineInputBorder(
     borderRadius: BorderRadius.circular(16),
     borderSide: const BorderSide(color: lightOrangeColor),
@@ -51,5 +55,63 @@ final class LabClinicasTheme {
       ),
     ),
   );
-  static final darkTheme = lightTheme;
+
+  static final darkTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: darkBackgroundColor),
+    useMaterial3: true,
+    fontFamily: 'Montserrat',
+    scaffoldBackgroundColor: darkBackgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: darkCardColor,
+      labelStyle: const TextStyle(
+        fontSize: 14,
+        color: orangeColor,
+        fontWeight: FontWeight.w700,
+      ),
+      floatingLabelStyle: const TextStyle(
+        color: blueColor,
+        fontWeight: FontWeight.w600,
+      ),
+      enabledBorder: _defaultInputBorder,
+      focusedBorder: _defaultInputBorder,
+      errorBorder: _defaultInputBorder.copyWith(
+        borderSide: const BorderSide(
+          color: Colors.red,
+        ),
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: blueColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        textStyle: const TextStyle(
+          fontFamily: 'Montserrat',
+          fontSize: 14,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    ),
+  );
+
+  static const titleStyle = TextStyle(
+    color: blueColor,
+    fontSize: 32,
+    fontWeight: FontWeight.w900,
+  );
+
+  static const titleSmallStyle = TextStyle(
+    color: blueColor,
+    fontSize: 24,
+    fontWeight: FontWeight.w900,
+  );
+
+  static const subTitleSmallStyle = TextStyle(
+    color: blueColor,
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
 }
