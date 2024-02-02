595366afcad3b84e08a2cede36c4fd6f0ad573f4
Matheus Hora <matheus11hora@gmail.com>
2024-02-02 02:48:04 -0300

    corrigindo erro

--- lab_clinicas_self_service/lib/src/module/auth/login/login_router.dart
+++ lab_clinicas_self_service/lib/src/module/auth/login/login_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 import 'package:lab_clinicas_self_service/src/module/auth/login/login_controller.dart';
 import 'package:lab_clinicas_self_service/src/module/auth/login/login_page.dart';
+import 'package:lab_clinicas_self_service/src/services/user_login_service.dart';
 import 'package:lab_clinicas_self_service/src/services/user_login_service_impl.dart';
 
 class LoginRouter extends FlutterGetItModulePageRouter {
@@ -9,7 +10,7 @@ class LoginRouter extends FlutterGetItModulePageRouter {
 
   @override
   List<Bind<Object>> get bindings => [
-        Bind.lazySingleton<UserLoginServiceImpl>(
+        Bind.lazySingleton<UserLoginService>(
             (i) => UserLoginServiceImpl(userRepository: i())),
         Bind.lazySingleton((i) => LoginController(loginService: i())),
       ];
