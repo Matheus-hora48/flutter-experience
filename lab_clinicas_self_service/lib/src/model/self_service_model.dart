6337f0ff23c7a77060fe357cf7ffe479fb29e0f2
Matheus Hora <matheus11hora@gmail.com>
2024-02-03 00:51:51 -0300

    fazendo a tela de who i am

---
+++ lab_clinicas_self_service/lib/src/model/self_service_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SelfServiceModel {
+  final String? name;
+  final String? lastName;
+
+  SelfServiceModel({
+    required this.name,
+    required this.lastName,
+  });
+
+  SelfServiceModel clear() {
+    return copyWith(
+      name: () => null,
+      lastName: () => null,
+    );
+  }
+
+  SelfServiceModel copyWith({
+    ValueGetter<String?>? name,
+    ValueGetter<String?>? lastName,
+  }) {
+    return SelfServiceModel(
+      name: name != null ? name() : this.name,
+      lastName: lastName != null ? lastName() : this.lastName,
+    );
+  }
+}
--- lab_clinicas_self_service/lib/src/module/self_service/who_i_am/who_i_am_page.dart
+++ lab_clinicas_self_service/lib/src/module/self_service/who_i_am/who_i_am_page.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
 import 'package:lab_clinicas_core/lab_clinicas_core.dart';
+import 'package:lab_clinicas_self_service/src/module/self_service/self_service_controller.dart';
+import 'package:validatorless/validatorless.dart';
 
-class WhoIAmPage extends StatelessWidget {
+class WhoIAmPage extends StatefulWidget {
   const WhoIAmPage({super.key});
+
+  @override
+  State<WhoIAmPage> createState() => _WhoIAmPageState();
+}
+
+class _WhoIAmPageState extends State<WhoIAmPage> {
+  final selfServiceController = Injector.get<SelfServiceController>();
+
+  final formKey = GlobalKey<FormState>();
+  final nameEC = TextEditingController();
+  final lastNameEC = TextEditingController();
+
+  @override
+  void dispose() {
+    nameEC.dispose();
+    lastNameEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +48,7 @@ class WhoIAmPage extends StatelessWidget {
           _,
           constraints,
         ) {
+          var sizeOf = MediaQuery.sizeOf(context);
           return SingleChildScrollView(
             child: Container(
               constraints: BoxConstraints(
@@ -40,8 +62,59 @@ class WhoIAmPage extends StatelessWidget {
               ),
               child: Center(
                 child: Container(
-                  decoration: const BoxDecoration(
+                  padding: const EdgeInsets.all(40),
+                  width: sizeOf.width * .8,
+                  decoration: BoxDecoration(
                     color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        Image.asset('assets/images/logo_vertical.png'),
+                        const SizedBox(
+                          height: 48,
+                        ),
+                        const Text(
+                          'Bem-Vindo!',
+                          style: LabClinicasTheme.titleStyle,
+                        ),
+                        const SizedBox(
+                          height: 48,
+                        ),
+                        TextFormField(
+                          controller: nameEC,
+                          validator: Validatorless.required('Nome obrigatório'),
+                          decoration: const InputDecoration(
+                            label: Text('Digite seu nome'),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        TextFormField(
+                          controller: lastNameEC,
+                          validator: Validatorless.required('Sobrenome obrigatório'),
+                          decoration: const InputDecoration(
+                            label: Text('Digite seu sobrenome'),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        SizedBox(
+                          width: sizeOf.width * .8,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              final valid = formKey.currentState?.validate() ?? false;
+                              
+                            },
+                            child: const Text('CONTINUAR'),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
