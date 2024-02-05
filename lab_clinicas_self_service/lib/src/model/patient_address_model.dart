08f58b39f34ab0cf16fbd9f0e7c1d0485c47e69f
Matheus Hora <matheus11hora@gmail.com>
2024-02-05 14:05:44 -0300

    criando as models

--- lab_clinicas_self_service/lib/src/model/patient_address_model.dart
+++ lab_clinicas_self_service/lib/src/model/patient_address_model.dart
@@ -1,1 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:json_annotation/json_annotation.dart';
 
+part 'patient_address_model.g.dart';
+
+@JsonSerializable()
+class PatientAddressModel {
+  final String cep;
+  final String streetAddress;
+  final String state;
+  final String city;
+  final String district;
+  PatientAddressModel({
+    required this.cep,
+    required this.streetAddress,
+    required this.state,
+    required this.city,
+    required this.district,
+  });
+
+  factory PatientAddressModel.fromJson(Map<String, dynamic> json) =>
+      _$PatientAddressModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PatientAddressModelToJson(this);
+}
--- lab_clinicas_self_service/lib/src/model/patients_model.dart
+++ lab_clinicas_self_service/lib/src/model/patients_model.dart
@@ -1,18 +1,36 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:json_annotation/json_annotation.dart';
+import 'package:lab_clinicas_self_service/src/model/patient_address_model.dart';
 
 part 'patients_model.g.dart';
 
 @JsonSerializable()
 class PatientsModel {
   final String id;
-  final String name,
-  final String email,
+  final String name;
+  final String email;
   @JsonKey(name: 'phone_number')
-  final string phoneNumber;
-  final String document,
-  
+  final String phoneNumber;
+  final String document;
+  final PatientAddressModel address;
+  @JsonKey(name: 'guardian', defaultValue: '')
+  final String guardian;
+  @JsonKey(name: 'guardian_identification_number', defaultValue: '')
+  final String guardianIdentificationNumber;
 
-  factory PatientsModel.fromJson(Map<String, dynamic> json) => _$PatientsModelFromJson(json);
+  PatientsModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+    required this.document,
+    required this.address,
+    required this.guardian,
+    required this.guardianIdentificationNumber,
+  });
 
-  Map<String,dynamic> toJson() => _$PatientsModelToJson(this);
+  factory PatientsModel.fromJson(Map<String, dynamic> json) =>
+      _$PatientsModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PatientsModelToJson(this);
 }
