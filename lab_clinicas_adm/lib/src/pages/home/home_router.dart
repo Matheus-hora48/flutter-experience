024b5c96fc7e48c690436511cdc9abfcd8b6128a
Matheus Hora <matheus11hora@gmail.com>
2024-06-02 16:53:14 -0300

    ✨ feat: Ajustes na home para chamar a pagina de checkin

--- lab_clinicas_adm/lib/src/pages/home/home_router.dart
+++ lab_clinicas_adm/lib/src/pages/home/home_router.dart
@@ -2,18 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 import 'package:lab_clinicas_adm/src/pages/home/home_controller.dart';
 import 'package:lab_clinicas_adm/src/pages/home/home_page.dart';
-import 'package:lab_clinicas_adm/src/repositories/attendant_desk_assignment/attendant_desk_assignment_repository.dart';
-import 'package:lab_clinicas_adm/src/repositories/attendant_desk_assignment/attendant_desk_assignment_repository_impl.dart';
 
 class HomeRouter extends FlutterGetItPageRouter {
   const HomeRouter({super.key});
 
   @override
   List<Bind<Object>> get bindings => [
-        Bind.lazySingleton<AttendantDeskAssignmentRepository>(
-          (i) => AttendantDeskAssignmentRepositoryImpl(restClient: i()),
-        ),
-        Bind.lazySingleton((i) => HomeController(attendantDeskRepository: i()))
+        Bind.lazySingleton((i) => HomeController(
+              attendantDeskRepository: i(),
+              callNextPatientService: i(),
+            ))
       ];
 
   @override
